90b8bddbd744cf1e81df6802b911561ce2115a24
Jocy Gonçalves Neto <jocygneto@gmail.com>
2024-02-27 19:34:52 +0000

    modificando o teste para mostrar a conexao com banco de dados

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
+import database from "../../../../infra/database.js";
+
 //api/status
-function status(request, response) {
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 AS sum;");
+  console.log(result.rows);
   response.status(200).json({ chave: "está funcionando óhj!" });
 }
 
