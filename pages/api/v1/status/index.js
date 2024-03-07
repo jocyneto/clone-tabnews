eb1c69b266d1336629fc9236b3e62d8ebcf12546
Jocy Gonçalves Neto <jocygneto@gmail.com>
2024-03-07 00:05:07 +0000

    creating api status like `version`, `max connections database` and `active connections database`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,9 +2,31 @@ import database from "infra/database.js";
 
 //api/status
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "está funcionando óhj!" });
+  const updateAt = new Date().toISOString();
+  const databaseVersion = await database.query("SHOW server_version;");
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+  const databaseActiveConnections = await database.query(
+    "SELECT COUNT(*) as active_connections FROM pg_stat_activity WHERE state = 'active'",
+  );
+
+  const databaseVersionResult = databaseVersion.rows[0].server_version;
+
+  const databaseMaxConnectionsResult =
+    databaseMaxConnections.rows[0].max_connections;
+
+  const databaseActiveConnectionsResult =
+    databaseActiveConnections.rows[0].active_connections;
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionResult,
+        max_connections: databaseMaxConnectionsResult,
+        active_connections: databaseActiveConnectionsResult,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,4 +2,25 @@ test("GET to api/v1/status should return 200", async () => {
   //é igual ao CURL o fetch
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  const rbUpdateAt = responseBody.update_at;
+  const rbDatabaseVersion = responseBody.dependencies.database.version;
+  const rbMaxConnections = responseBody.dependencies.database.max_connections;
+  const rbActiveConnections =
+    responseBody.dependencies.database.active_connections;
+
+  expect(rbUpdateAt).toBeDefined();
+  expect(rbDatabaseVersion).toBeDefined();
+  expect(rbMaxConnections).toBeDefined();
+  expect(rbActiveConnections).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.update_at).toISOString();
+  expect(rbUpdateAt).toEqual(parsedUpdatedAt);
+  expect(rbDatabaseVersion).toEqual("16.0");
 });
+
+/*
+ler a implementacao da documentacao node-postgres
+verificar o database.js
+*/
